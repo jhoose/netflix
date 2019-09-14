fac637078a474e3161366c9d69e69e83afda52dc
jhoosebusiness <32284149+jhoosebusiness@users.noreply.github.com>
2019-09-14 17:42:56 -0400

    Begin Angular 8 IBM Netflix Stranger Things TV show App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { InspirationComponent } from './inspiration/inspiration.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EpisodesComponent } from './episodes/episodes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InspirationComponent,
+    GalleryComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule
