ab2ffe272dec2966380922b749a5f7f68f3ea5fb
jhoosebusiness <32284149+jhoosebusiness@users.noreply.github.com>
2019-09-15 20:34:06 -0400

    Created loop for Episode JSON data, Added Sorting, Added ngIf filtering by Season, Added Star FontAwesome icon

--- src/app/episodes/episodes.component.ts
+++ src/app/episodes/episodes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-episodes',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodesComponent implements OnInit {
 
+  faStar = faStar;
+
+  @Input() lang: any;
+
   constructor() { }
 
   ngOnInit() {
+
+    // const episodesArray = this.lang.episodeList;
+
+    // console.log('Episodes Array:');
+    // console.log(episodesArray);
+
+    // episodesArray.sort((a: any, b: any) => {
+    //   return a.chapter - b.chapter;
+    // });
+
+  }
+
+  get sortedEpisodes() {
+    const episodesArray = this.lang.episodeList;
+
+    return episodesArray.sort((a: any, b: any) => {
+      return a.chapter - b.chapter;
+    });
   }
 
 }
